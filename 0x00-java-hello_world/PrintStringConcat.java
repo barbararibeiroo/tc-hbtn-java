6d2875372ac1b256a337e8d66ff2077d21fa1813
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-26 12:55:02 -0300

    PrintStringConcat

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,9 @@
+
+public class PrintStringConcat {
+
+	public static void main(String[] args) {
+		String texto1 = "Holberton";
+		String texto2 = "School!";
+		String texto3 = "Holberton" + " School!";		
+		System.out.print("Bem vindo a " + texto3);
+}}
