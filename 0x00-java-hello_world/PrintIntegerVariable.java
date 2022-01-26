a9b8cf90d85b9e375c2db9469fc1142df3482ee1
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-26 10:29:40 -0300

    first commit

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,5 +1,5 @@
 public class PrintIntegerVariable {
 	public static void main(String[] args) {
-		String txt = "Avenida Andalo, ";
+		String txt = "Avenida Andaló, ";
 		int numero = 311;
 	System.out.println(txt + numero);}}
