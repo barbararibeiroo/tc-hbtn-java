f0c5cdcade1fc988ed6e6b878994dd91d34d333f
barbararibeiroo <43424379+barbararibeiroo@users.noreply.github.com>
2022-01-26 13:43:46 -0300

    Update PrintIntegerVariable.java

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,5 +1,5 @@
 public class PrintIntegerVariable {
 	public static void main(String[] args) {
-		String txt = "Avenida Andalo, ";
+		String txt = "Avenida Andaló, ";
 		int numero = 311;
 	System.out.println(txt + numero);}}
