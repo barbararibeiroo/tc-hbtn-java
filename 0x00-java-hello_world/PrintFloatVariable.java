c2116724a67e6079859d4572936d77528d9217f8
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-26 10:54:38 -0300

    first commit

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,7 +1,7 @@
 
 public class PrintFloatVariable {
 	  public static void main(String[] args){
-		 double pi = (float) Math.PI;
+		 double pi = 3.1424259f;
 	System.out.printf("O valor de PI é: "  + "%.3f\n", pi);
 	  }
 }
