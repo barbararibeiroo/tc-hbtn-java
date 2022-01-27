c514a06cb3c4e6a9ce4af22865b29247a913f245
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-27 00:47:45 -0300

    0x01

---
+++ 0x01/maisculo/Caractere.java
@@ -0,0 +1,10 @@
+
+public class Caractere {
+
+	public static boolean EhMaiusculo(char c) {
+		// DE 65 A 90 NA TABELA ASCII SÃO REPRESENTADOS POR LETRA MAIUSCULA.
+		return(c >= 65 && c <= 90);
+		}
+	}
+
+
