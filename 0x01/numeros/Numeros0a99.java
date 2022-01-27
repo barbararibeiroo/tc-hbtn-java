2e668599f8916ce968021514a8d8df7bd439a7a9
barbararibeiroo <43424379+barbararibeiroo@users.noreply.github.com>
2022-01-27 01:17:26 -0300

    Update Numeros0a99.java

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -4,11 +4,13 @@ public class Numeros0a99 {
 
 	public static void main(String[] args) {
 		
-		for(int i=0; i<=99; i++) {
-			System.out.print(i + ", ");
-		}
-		
-		
+		for(int i=0; i<=99; i++) 
+			if(i <=98) {
+				System.out.print(i + ", " );
+			}
+			else {
+				System.out.print(i);
+			}
 	}
-	;
 }
+	
