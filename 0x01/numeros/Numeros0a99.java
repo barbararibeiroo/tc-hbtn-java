2ed7c36ed996dc3f08208c1ca12e6f92d694594b
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-27 09:25:00 -0300

    Numeros0a99

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
+package controle;
 
-
-=======
->>>>>>> eb4a8878d0a233c77a55dfc1d30e706034d5c2f9
 public class Numeros0a99 {
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		for(int i=0; i<=99; i++) 
-=======
-		for(int i=0; i<=100; i++) 
->>>>>>> eb4a8878d0a233c77a55dfc1d30e706034d5c2f9
-			if(i <=98) {
-				System.out.print(i + ", " );
-=======
-		public static void main(String[] args) {
 		for(int i=0; i<100; i++) 
 			if(i <= 98) {
 				System.out.print(i + ", ");
->>>>>>> 4ca38bb80be392e3039ad8757f27c21d6d0a0c58
 			}
 			else {
-				System.out.println(i);
+				System.out.print(i);
 			}
 	}
 }
 	
-<<<<<<< HEAD
 	
 		
 
-=======
->>>>>>> eb4a8878d0a233c77a55dfc1d30e706034d5c2f9
