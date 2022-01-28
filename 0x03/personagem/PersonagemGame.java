a3f7c7c6d2237081df4ae82698db1fecdd73e9c0
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-28 13:01:53 -0300

    personagem

---
+++ 0x03/personagem/PersonagemGame.java
@@ -0,0 +1,29 @@
+
+public class PersonagemGame {
+
+	int saudeAtual;
+	String nome;
+	
+	public PersonagemGame(int saudeAtual, String nome) {
+		super();
+		this.saudeAtual = saudeAtual;
+		this.nome = nome;
+	}
+
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
