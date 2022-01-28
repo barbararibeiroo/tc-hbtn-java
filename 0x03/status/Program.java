d1d4a273440e5aaac31a99e1828b61ee1f826ebc
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-28 17:18:47 -0300

    status atualizado

--- 0x03/status/Program.java
+++ 0x03/status/Program.java
@@ -1,31 +1,29 @@
-package status;
-
-import consistencia.PersonagemGame;
 
 public class Program {
-	  public static void main(String[] args) {  
-	        PersonagemGame personagem1 = new PersonagemGame();
 
-	        personagem1.setNome("Sonic");
-	        personagem1.setSaudeAtual(0);
+	public static void main(String[] args) {
+		PersonagemGame personagem1 = new PersonagemGame();
 
-	        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(0);
 
-	        personagem1.receberCura(90);
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-	        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+        personagem1.receberCura(90);
 
-	        personagem1.tomarDano(25);
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-	        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+        personagem1.tomarDano(25);
 
-	        personagem1.tomarDano(80);
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-	        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+        personagem1.tomarDano(80);
 
-	        personagem1.receberCura(10);
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-	        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
-	      }
-	   }
-	   
+        personagem1.receberCura(10);
+
+        System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+	}
+
+}
