b1b476596aeff91780098d3b4acf8e251ab4a844
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-28 15:15:27 -0300

    metodos

---
+++ 0x03/metodos/PersonagemGame.java
@@ -0,0 +1,43 @@
+package controle;
+
+public class PersonagemGame {
+
+	private int saudeAtual;
+	private String nome;
+
+	
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void receberCura(int quantidadeDeCura) {
+		this.saudeAtual = saudeAtual + quantidadeDeCura; 
+		this.setSaudeAtual(saudeAtual);
+		if(saudeAtual > 100) {
+			this.saudeAtual = 100;
+		}
+		
+	}
+
+	public void tomarDano(int quantidadeDeDano) {
+		this.saudeAtual = saudeAtual - quantidadeDeDano; 
+		this.setSaudeAtual(saudeAtual);
+		if(saudeAtual < 0) {
+			this.saudeAtual = 0;
+		}
+		
+	}
+
+}
