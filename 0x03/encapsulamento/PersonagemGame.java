d4c106c33d41a21c37232b098f7bc291e6c342dd
barbararibeiroo <barbararibeiro18@gmail.com>
2022-01-28 14:21:11 -0300

    encapsulamento

---
+++ 0x03/encapsulamento/PersonagemGame.java
@@ -0,0 +1,30 @@
+package controle;
+
+public class PersonagemGame {
+
+	private int saudeAtual;
+	private String nome;
+
+	public PersonagemGame(int saudeAtual, String nome) {
+		super();
+		this.saudeAtual = saudeAtual;
+		this.nome = nome;
+	}
+
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
---
+++ 0x03/encapsulamento/Program.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class Program {
+    public static void main(String[] args) {
+        PersonagemGame personagem1 = new PersonagemGame(0, null);
+
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(10);
+
+        System.out.printf("Nome: %s - Saude: %d\n", personagem1.getNome(), personagem1.getSaudeAtual());
+    }
+}
