0865ef4829533edfeea6721a48cb3ad9408b7b36
barbararibeiroo <43424379+barbararibeiroo@users.noreply.github.com>
2022-01-31 09:54:23 -0300

    Update FormaGeometrica.java

--- 0x04/abstrata/FormaGeometrica.java
+++ 0x04/abstrata/FormaGeometrica.java
@@ -1,6 +1,6 @@
 
 public abstract class FormaGeometrica {
-	public abstract double area() {
+	public double area() {
 		throw new UnsupportedOperationException("Retangulo eh uma subclasse de FormaGeometrica");
 	}
 
