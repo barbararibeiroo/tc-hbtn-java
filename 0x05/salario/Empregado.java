e44396c67b46906808509fed8013cf6ba34b1fa7
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-01 11:54:39 -0300

    0x05

---
+++ 0x05/salario/Empregado.java
@@ -0,0 +1,28 @@
+
+public class Empregado  {
+	public double salarioFixo;
+
+	public Empregado(double salarioFixo) {
+		this.salarioFixo = salarioFixo;
+	}
+
+	public double getSalarioFixo() {
+		return salarioFixo;
+	}
+
+	public void setSalarioFixo(double salarioFixo) {
+		this.salarioFixo = salarioFixo;
+	}
+
+	public  double calcularBonus(Departamento departamento) {
+		if (departamento.alcancouMeta() == true) {
+		 return salarioFixo * 0.1f; 
+		 }
+		else {
+			return 0.0f;
+		}
+	}	
+	double calcularSalarioTotal(Departamento departamento) {
+		return calcularBonus(departamento) + salarioFixo;
+	}
+}
