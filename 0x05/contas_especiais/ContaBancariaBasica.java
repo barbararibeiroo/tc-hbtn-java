1cdb037da3f731c2347ca94a86229e0bad2152b0
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-01 21:58:55 -0300

    conta_basica

--- 0x05/contas_especiais/ContaBancariaBasica.java
+++ 0x05/contas_especiais/ContaBancariaBasica.java
@@ -2,39 +2,33 @@
 import exceptions.OperacaoInvalidaException;
 
 public class ContaBancariaBasica {
-
-	protected String numeracao;
-	public double saldo;
+	private String numeracao;
+	protected double saldo;
 	private double taxaJurosAnual;
-
-	public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {
+	
+	public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {	
 		this.numeracao = numeracao;
 		this.saldo = 0;
 		this.taxaJurosAnual = taxaJurosAnual;
-
 	}
-
 	public String getNumeracao() {
 		return numeracao;
 	}
-
 	public double getSaldo() {
 		return saldo;
 	}
-
 	public double getTaxaJurosAnual() {
 		return taxaJurosAnual;
 	}
-
-	public void depositar(double valor) throws OperacaoInvalidaException {
-		if (valor > 0) {
+	void depositar(double valor) throws OperacaoInvalidaException {
+		if(valor > 0) {
 			this.saldo = saldo + valor;
-		} else {
+		}else {
 			throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
 		}
 	}
-
-	public void sacar(double valor) throws OperacaoInvalidaException {
+	void sacar(double valor) throws OperacaoInvalidaException {
+		
 		if (valor < 0) {
 			throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
 		} else if (saldo < valor) {
@@ -42,8 +36,8 @@ public class ContaBancariaBasica {
 		} else {
 			saldo -= valor;
 		}
-	}
-
+	
+	}	
 	public double calcularTarifaMensal() {
 		if ((saldo * 0.1) < 10) {
 			return saldo * 0.1;
@@ -51,7 +45,6 @@ public class ContaBancariaBasica {
 			return 10.00f;
 		}
 	}
-
 	public double calcularJurosMensal() {
 		if (saldo < 0) {
 			return 0.00f;
@@ -59,9 +52,8 @@ public class ContaBancariaBasica {
 			return saldo * ((taxaJurosAnual / 100) / 12);
 		}
 	}
-
-	public void aplicarAtualizacaoMensal() {
-		this.saldo = saldo - calcularTarifaMensal() + calcularJurosMensal();
-
-	}
+	void aplicarAtualizacaoMensal() {
+		this.saldo = saldo - calcularTarifaMensal() + calcularJurosMensal() ;
+	}	
 }
+
--- 0x05/contas_especiais/ContaBancariaTarifada.java
+++ 0x05/contas_especiais/ContaBancariaTarifada.java
@@ -2,33 +2,36 @@
 import exceptions.OperacaoInvalidaException;
 
 public class ContaBancariaTarifada extends ContaBancariaBasica {
-	
-	private int quantidadeTransacoes;
 
+	private int quantidadeTransacoes;
+	
 	public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
 		super(numeracao, taxaJurosAnual);
-		
+		// TODO Auto-generated constructor stub
 	}
-
+	
+	@Override
+	void depositar(double valor) throws OperacaoInvalidaException {
+		if(valor > 0) {
+			this.saldo = saldo + valor - 0.10;
+		}else {
+			throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+		}
+	}
 	public int getQuantidadeTransacoes() {
 		return quantidadeTransacoes;
 	}
 	@Override
-	public void depositar(double valor) throws OperacaoInvalidaException {
-		if (valor > 0) {
-			this.saldo = saldo + valor - 0.10;
-		} else {
-			throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
-		}
-	}
-	@Override
-	public void sacar(double valor) throws OperacaoInvalidaException {
+	void sacar(double valor) throws OperacaoInvalidaException {
+		
 		if (valor < 0) {
 			throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
 		} else if (saldo < valor) {
 			throw new OperacaoInvalidaException("Valor de saque deve ser maior que o saldo atual");
 		} else {
-			saldo -= valor - 0.10;
+			saldo = saldo - valor - 0.10;
 		}
-	}
+	
+	}	
+
 }
