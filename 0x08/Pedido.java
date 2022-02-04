51c46db5cc3dbe3da97fa13c7ed06e361bc66569
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-04 14:46:40 -0300

    0x08

---
+++ 0x08/Pedido.java
@@ -0,0 +1,42 @@
+
+public class Pedido {
+
+	
+	private int percentualDesconto;
+	private ItemPedido[] itens;
+	private double total;
+	
+	public Pedido() {
+	
+	}
+	
+	public Pedido(int percentualDesconto, ItemPedido[] itens) {
+		this.percentualDesconto = percentualDesconto;
+		this.itens = itens;
+	}
+	
+	
+	public int getPercentualDesconto() {
+		return percentualDesconto;
+	}
+
+	public void setPercentualDesconto(int percentualDesconto) {
+		this.percentualDesconto = percentualDesconto;
+	}
+
+	public ItemPedido[] getItens() {
+		return itens;
+	}
+
+	public void setItens(ItemPedido[] itens) {
+		this.itens = itens;
+	}
+
+	public double calcularTotal() {
+		for (ItemPedido item : itens) {
+			total += (item.getProduto().obterPrecoLiquido(item.getProduto().getPrecoBruto())*item.getQuantidade());
+		}
+		
+		return total - (total * percentualDesconto / 100 );
+	}
+}
