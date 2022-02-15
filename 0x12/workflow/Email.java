2e31fbbb73105308c04cebdaac85b8b815c8da73
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-15 12:11:21 -0300

    0x12

---
+++ 0x12/workflow/Email.java
@@ -0,0 +1,12 @@
+
+public class Email implements CanalNotificacao {
+
+	@Override
+	public void notificar(Mensagem mensagem) {
+		System.out.println("[EMAIL] {" + mensagem.getTipoMensagem() + "} - " + mensagem.getTexto());
+		
+	}
+
+	
+
+}
