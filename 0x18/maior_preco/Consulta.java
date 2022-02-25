3afecbc8b74253d712bc1e6badeddaf4f82ffaab
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-25 14:56:51 -0300

    0x18

--- 0x18/maior_preco/Consulta.java
+++ 0x18/maior_preco/Consulta.java
@@ -1,5 +1,8 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import preco_minimo.CategoriaProduto;
 
 public class Consulta {
 
@@ -9,6 +12,14 @@ public class Consulta {
 
 		return pedido.getProdutos().stream().filter(t -> t.getCategoria().equals(CategoriaProduto.LIVRO))
 				.collect(Collectors.toList());
+
+	}
+	// Ordene a lista de produtos por preco com order reversa e obtenha o primeiro
+	// produto.
+	// Utilize Comparator.comparing com reversed
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+		return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst().orElse(null);
 
 	}
 
