c89b32918522b438039ea4cd24ac5ce40956beed
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-25 15:10:49 -0300

    0x18

--- 0x18/maior_preco/Consulta.java
+++ 0x18/maior_preco/Consulta.java
@@ -10,6 +10,7 @@ public class Consulta {
 
 	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
 
+
 		return pedido.getProdutos().stream().filter(t -> t.getCategoria().equals(CategoriaProduto.LIVRO))
 				.collect(Collectors.toList());
 
