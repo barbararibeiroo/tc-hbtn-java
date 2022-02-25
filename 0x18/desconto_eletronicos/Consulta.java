8ab70af2ffc0db5dbff4c861092f524685132e12
barbararibeiroo <barbararibeiro18@gmail.com>
2022-02-25 16:50:54 -0300

    descontos_eletronicos

--- 0x18/desconto_eletronicos/Consulta.java
+++ 0x18/desconto_eletronicos/Consulta.java
@@ -2,7 +2,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import br.com.alura.Aula;
 import preco_minimo.CategoriaProduto;
 
 public class Consulta {
