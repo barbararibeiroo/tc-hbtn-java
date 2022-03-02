69396f62303d035ecc68b9ebdf238a4cbd3f0778
barbararibeiroo <barbararibeiro18@gmail.com>
2022-03-02 13:32:01 -0300

    0x19

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,43 @@
+
+import java.util.List;
+import java.util.Map;
+import java.util.TooManyListenersException;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+	/*
+	 * obterPessoasPorCargo: recebe uma lista de Pessoa e retorna um Map<String,
+	 * List<Pessoa>> que contém o nome do cargo como chave do Map e a lista de
+	 * pessoas daquele cargo como valor do Map.
+	 */
+
+	public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> todasPessoas) {
+		return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+
+	}
+	// obterIdadePorCargo: recebe uma lista de Pessoa e retorna um Map<String,
+	// List<Integer>> que contém
+	// o nome do cargo como chave do Map e a lista de idades de pessoas daquele
+	// cargo como valor do Map.
+
+	public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> todasPessoas) {
+		return todasPessoas.stream().collect(
+				Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+
+	}
+
+	/*
+	 * obterPessoasPorCargoAcimaDe40anos: recebe uma lista de Pessoa e retorna um
+	 * Map<String, List<Pessoa>> que contém o nome do cargo como chave do Map e a
+	 * lista de pessoas daquele cargo e que possuam idade acima de 40 anos como
+	 * valor do Map.
+	 */
+
+	public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> todasPessoas) {
+		return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+				Collectors.filtering(i -> i.getIdade() >= 40, Collectors.toList())));
+	}
+
+}
