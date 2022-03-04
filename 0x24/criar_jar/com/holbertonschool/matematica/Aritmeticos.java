27e965ab18cea7246e842134f874e1b7296937fc
barbararibeiroo <barbararibeiro18@gmail.com>
2022-03-04 16:50:37 -0300

    0x24

---
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,19 @@
+
+public class Aritmeticos {
+
+	public int somar(int val1,int val2) {
+		return val1 + val2;
+	}
+	
+	public int subtrair(int val1,int val2) {
+		return val1 + val2;
+	}
+	
+	public int multiplicar(int val1,int val2) {
+		return val1 + val2;
+	}
+	public int dividir(int val1,int val2) {
+		return val1 + val2;
+	}
+	
+}
